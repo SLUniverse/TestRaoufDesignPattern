e96a361fdf2b4adccc5c0f925de85fcd304a2d09
SLUniverse <dkicidy@gmail.com>
2024-11-12 20:21:00 -0500

    initial commit

---
+++ src/main/java/edu/bdeb/a13/Main.java
@@ -0,0 +1,18 @@
+package edu.bdeb.a13;
+
+/*
+*desciption de l classe
+*
+*@version 1.0
+*@see uneAutreClasse
+*@author s lee
+*
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+}
